9e72a10a829fa414a79d3c7e48cd811e28e36f67
jxd136 <jxd24@mails.tsinghua.edu.cn>
2025-08-31 14:19:17 +0800

    first

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,1 +1,4 @@
 # Add your commands here
+docker built -t hw_copy_app -f hw_copy/Dockerfile .
+doker run --rm hw_copy_app ./add 4 5
+docker run --rm hw_copy_app ./mul 7 8
--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,1 +1,4 @@
 # Add your commands here
+docker build -t hw_vol -f hw_vol/Dockerfile .
+docker run --rm -v "$(pwd)/src":/source hw_vol sh -c "g++ add.cpp -o add&& ./add 4 5"
+docker run --rm -v "$(pwd)/src":/source hw_vol sh -c "g++ mul.cpp -o add&& ./mul 4 5"
